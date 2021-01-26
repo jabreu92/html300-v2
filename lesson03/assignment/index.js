be50edbd04ddb1aba37bcd5e5239e698213e660e
jabreu92 <jose.abreu4@hotmail.com>
2021-01-25 23:40:12 -0800

    Need to check to to read json file

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -1,11 +1,15 @@
 //How can I import local JSON file into here ? I tried a required() function but that is from node js. ?
 const jsonDATA = '[{"name":"Paolo Maldini","jobTitle":"Front End Developer","company":"Web Weavers","experience":"3 years","school":"University of Washington","major":"Marketing","email":"paolo@example.com","linkedInUrl":"paolo.linkedinprofile.com","codeLanguages":["HTML","CSS","JavaScript","Node","Express"]},{"name":"Barbara Bonansea","jobTitle":"Software Engineer","company":"Excellence in the Cloud","experience":"12 years","school":"University of Southern California","major":"Computer Science","email":"barbara@example.com","linkedInUrl":"barbara.linkedinprofile.com","codeLanguages":["HTML","JavaScript","C","Go"]},{"name":"Javier Hernandez","jobTitle":"User Experience Engineer","company":"Web Sites and More","experience":"5 years","school":"Seattle University","major":"Performing Arts","email":"javier@example.com","linkedInUrl":"javier.linkedinprofile.com","codeLanguages":["HTML","CSS"]},{"name":"Maribel Dominguez","jobTitle":"Front End Engineer","company":"Bits and Bytes","experience":"6 years","school":"University of Washington","major":"Mechanical Engineering","email":"maribel@example.com","linkedInUrl":"maribel.linkedinprofile.com","codeLanguages":["HTML","CSS","JavaScript","React","Vue","Redux"]}]'
 const persons = JSON.parse(jsonDATA);
-console.log(persons);
+
 
 const container = document.querySelector('.template-hook');
 
     let personsHTML = persons.map(function(el) {
+        
+        let languages = [];
+        el.codeLanguages.forEach(element => languages.push(element));
+        
         let person =`<div class="container">
         <div class="employee-card-left">
            <img class="employee-card-left___image" src="img/headshot.jpg" alt="Avatar" style="width:100%">
@@ -16,6 +20,7 @@ const container = document.querySelector('.template-hook');
            <div class="employee-card-right___info">
               <p> <strong> Company: </strong>${el.company}</p>
               <p> <strong> Experience: </strong>${el.experience}</p>
+              <p> <strong> Languages: </strong>${languages.toString()}</p>
               <p> <strong> School: </strong>${el.school}</p>
               <p> <strong> Major: </strong>${el.major}</p>
               <p> <strong> Email: </strong>${el.email}</p>
@@ -26,7 +31,7 @@ const container = document.querySelector('.template-hook');
        
        return person;
        });
-      
+  
     let list = document.createElement('article');
     list.innerHTML = personsHTML;
     container.append(list);
