5839cb523d81e6b35faf63520c3667dcbf2c11c3
jabreu92 <jose.abreu4@hotmail.com>
2021-03-01 15:24:55 -0800

    project structure

---
+++ lesson07/assignment/vue-cli/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
