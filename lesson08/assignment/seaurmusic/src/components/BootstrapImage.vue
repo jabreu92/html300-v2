76fbd5823af9751f3e0424a2610f917f182113dc
jabreu92 <jose.abreu4@hotmail.com>
2021-03-09 21:05:27 -0800

    added mixin

--- lesson08/assignment/seaurmusic/src/components/BootstrapImage.vue
+++ lesson08/assignment/seaurmusic/src/components/BootstrapImage.vue
@@ -1,10 +1,35 @@
 <template>
    <div>
-    <b-img thumbnail :src="imageSrcUrl" fluid :alt="imageAlt" :title="imageTitle"></b-img>
+    <b-img thumbnail :rounded="imageBorder" :src="imageSrcUrl" fluid :alt="imageAlt" :title="imageTitle" @click="modifyBorder"></b-img>
   </div>
 </template>
 
 <script>
+
+const imageMixin = { 
+
+  data (){
+    return {
+      imageBorder: '0',
+      isRounded: false
+    }
+
+  },
+  methods: {
+    modifyBorder () {
+      this.isRounded = !this.isRounded
+
+      if(this.isRounded) {
+        this.imageBorder = 'circle'
+      }else {
+        this.imageBorder = '0'
+      }
+
+    }
+  }
+}
+
+
 export default {
   props: {
     imageSrcUrl: {
@@ -14,15 +39,17 @@ export default {
     },
     imageAlt: {
       type: String,
-      required: true,
+      required: false,
       default: 'Event Image Goes Here'
     },
     imageTitle: {
       type: String,
-      required: true,
+      required: false,
       default: 'Event Title Goes Here'
     }
-  }
+  }, 
+  mixins: [imageMixin]
+  
 
 }
 </script>
